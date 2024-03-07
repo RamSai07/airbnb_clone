ca94a9ab2d243bb591b17d7f36ca52253d8cc982
ramsai <ramsaiandhavarapu07@gmail.com>
2024-03-07 21:49:33 +0530

    major changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom";
 import Main from "./Components/Home/Main";
 import PropertyList from './Components/Home/PropertyList';
+import PropertyDetails from "./Components/PropertyDetails/PropertyDetails";
 function App() {
   //manages the  routes of our app using createBrowserRouter method which is a part of react-router-dom library
   const router =createBrowserRouter(
@@ -11,8 +12,13 @@ function App() {
       
       //routes are created by passing a path as the first parameter and what component should be rendered when that path
       //exact properties ensure that route matches exactly that u gave in path
-      <Route path="/" element={<Main/>}   id='main' exact>
-        <Route id='home' index element={<PropertyList/>} exact/> 
+      <Route path="/" element={<Main/>} id="main" exact>
+        <Route id="home" index element={<PropertyList/>} exact/> 
+        <Route element={<PropertyDetails/>}
+         id ="PropertyDetails"
+        path="propertylist/:id"
+        exact
+        />
       </Route>
       
 
