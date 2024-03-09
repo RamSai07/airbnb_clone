91886d8fbf3aafd1efa95c88aac4db0883c89bd5
ramsai <ramsaiandhavarapu07@gmail.com>
2024-03-09 20:58:38 +0530

    added new reducer in store.js

--- frontend/src/Store/store.js
+++ frontend/src/Store/store.js
@@ -1,11 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import propertySlice from "./Property/property-slice";
 import propertyDetailsSlice from "./PropertyDetails/propertyDetails-slics";
+import userSlice from "./User/user-slice";
 
 const store = configureStore({
     reducer:{
         properties:propertySlice.reducer,
         propertydetails:propertyDetailsSlice.reducer,
+        user:userSlice.reducer,
+
     },
 });
 
