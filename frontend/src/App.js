710010ed0b5e7fd61cd1fa22c812317c0fb9d149
ramsai <ramsaiandhavarapu07@gmail.com>
2024-03-14 20:53:42 +0530

    created ForgotPassword.ResetPassword.js and some changes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,85 @@
-import './App.css';
+import "./App.css";
 //importing neccesary components and function from the react-router-dom library for  routing purposes.
-import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Main from "./Components/Home/Main";
-import PropertyList from './Components/Home/PropertyList';
+import PropertyList from "./Components/Home/PropertyList";
 import PropertyDetails from "./Components/PropertyDetails/PropertyDetails";
-import {Flip,ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useEffect } from 'react';
-import { useDispatch,useSelector } from 'react-redux';
-import { currentUser } from './Store/User/user-action';
-import { userActions } from './Store/User/user-slice';
-import Login from './Components/User/Login';
-import Signup from './Components/User/Signup';
-import Profile from './Components/User/Profile';
-import EditProfile from './Components/User/EditProfile';
-import UpdatePassword from './Components/User/UpdatePassword';
+import { Flip, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { currentUser } from "./Store/User/user-action";
+import { userActions } from "./Store/User/user-slice";
+import Login from "./Components/User/Login";
+import Signup from "./Components/User/Signup";
+import Profile from "./Components/User/Profile";
+import EditProfile from "./Components/User/EditProfile";
+import UpdatePassword from "./Components/User/UpdatePassword";
+import ForgotPassword from "./Components/User/ForgotPassword";
+import ResetPassword from "./Components/User/ResetPassword";
 function App() {
   const dispatch = useDispatch();
-  const {errors}=useSelector((state)=>state.user);
-  useEffect(()=>{
-    if(errors){
+  const { errors } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (errors) {
       dispatch(userActions.clearError());
     }
     dispatch(currentUser());
-  },[errors,dispatch]);
+  }, [errors, dispatch]);
   //manages the  routes of our app using createBrowserRouter method which is a part of react-router-dom library
-  const router =createBrowserRouter(
+  const router = createBrowserRouter(
     //creates routes from the elements passed to it.
     createRoutesFromElements(
-      
       //routes are created by passing a path as the first parameter and what component should be rendered when that path
       //exact properties ensure that route matches exactly that u gave in path
-      <Route path="/" element={<Main/>} id="main" exact>
-        <Route id="home" index element={<PropertyList/>} exact/> 
-        <Route element={<PropertyDetails/>}
-         id ="PropertyDetails"
-        path="propertylist/:id"
-        exact
+      <Route path="/" element={<Main />} id="main" exact>
+        <Route id="home" index element={<PropertyList />} exact />
+        <Route
+          element={<PropertyDetails />}
+          id="PropertyDetails"
+          path="propertylist/:id"
+          exact
         />
-        <Route id='login' path='/login' element={<Login/>}></Route>
-        <Route id='signup' path='/signup' element={<Signup/>}></Route>
-        <Route id='profile' path='/profile' element={<Profile/>}></Route>
-        <Route id='editprofile' path='/editprofile' element={<EditProfile/>}></Route>
-        <Route id='updatepassword' path='/user/updatepassword' element={<UpdatePassword/>}></Route>
-
-
-
-
+        <Route id="login" path="/login" element={<Login />}></Route>
+        <Route id="signup" path="/signup" element={<Signup />}></Route>
+        <Route id="profile" path="/profile" element={<Profile />}></Route>
+        <Route
+          id="editprofile"
+          path="/editprofile"
+          element={<EditProfile />}
+        ></Route>
+        <Route
+          id="updatepassword"
+          path="/user/updatepassword"
+          element={<UpdatePassword />}
+        ></Route>
+        <Route
+          id="forgotpassword"
+          path="/user/forgetpassword"
+          element={<ForgotPassword />}
+        ></Route>
+        <Route
+          id="resetpassword"
+          path="/user/resetPassword/:token"
+          element={<ResetPassword />}
+        ></Route>
       </Route>
-      
-
     )
-  )
+  );
   return (
     <div className="App">
       {/* this ensure that the routing function is available throughout the application */}
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
       <ToastContainer
-      position='bottom-center'
-      autoClose={3000}
-      draggable={true}
-      transition={Flip}
+        position="bottom-center"
+        autoClose={3000}
+        draggable={true}
+        transition={Flip}
       />
     </div>
   );
---
+++ frontend/src/Components/User/ResetPassword.js
@@ -0,0 +1,61 @@
+import React,{useState} from 'react';
+import {useDispatch } from 'react-redux';
+import { useNavigate,useParams } from 'react-router-dom';
+import { resetPassword } from '../../Store/User/user-action';
+import {toast} from "react-toastify";
+
+const ResetPassword = () => {
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    const {token}=useParams();
+    const[password,setPassword]=useState("");
+    const[passwordConfirm,setPasswordConfirm]=useState("");
+
+    const submitHandler=(e)=>{
+        e.preventDefault();
+        if(password !== passwordConfirm){
+            return toast.error("Password does not matched");
+        }
+        dispatch(resetPassword({password,passwordConfirm},token));
+        toast.success('Password has been changed successfully');
+        navigate("/login");
+    };
+  return (
+    <>
+    <div className='row wrapper'>
+        <div className='col-10 col-lg-5'>
+            <form className='shadow-lg' onSubmit={submitHandler}>
+                <h1 className='password_title'>New Password</h1>
+                <div className='form-group'>
+                    <lable htmlfor="password_field">Password</lable>
+                    <input
+                    type='password'
+                    id='password_field'
+                    className='form-control'
+                    value={password}
+                    onChange={((e)=>setPassword(e.target.value))}
+                    />
+
+                </div>
+                <div className='form-group'>
+                    <lable htmlfor="confirm_password_field">Password</lable>
+                    <input
+                    type='password'
+                    id='confirm_password_field'
+                    className='form-control'
+                    value={passwordConfirm}
+                    onChange={((e)=>setPasswordConfirm(e.target.value))}
+                    />
+
+                </div>
+                <button type='submit' id='new_password_button' className='btn-block py-3 password-btn'>Set Password</button>
+
+            </form>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default ResetPassword;
